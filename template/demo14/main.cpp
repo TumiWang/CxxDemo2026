ad3d989a2e0d0241b99e10d5df88a744a17642a9
Tumi Wang From MacbookPro11.1 <lianyuguhun@gmail.com>
2026-07-17 16:55:26 +0800

    添加 数组作为函数参数的例子

--- template/demo14/main.cpp
+++ template/demo14/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-template<size_t N>
-void f(int (&array)[N]) {
-    for(size_t i = 0; i < N; ++i) {
-        std::cout << array[i] << " ";
-    }
-    std::cout << std::endl;
-}
+// template<size_t N>
+// void f(int (&array)[N]) {
+//     for(size_t i = 0; i < N; ++i) {
+//         std::cout << array[i] << " ";
+//     }
+//     std::cout << std::endl;
+// }
 
 template<class T, size_t N>
 void f(T (&array)[N]) {
